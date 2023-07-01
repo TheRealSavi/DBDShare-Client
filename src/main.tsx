597c256fc5c812c9d62bb3d3160dae249c269ec2
John Gibbons <johngibbons167@gmail.com>
2023-07-01 15:19:30 -0400

    resolved linting errors from using ecmascript

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import { UserProvider } from "./components/UserContext.tsx";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
     <UserProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </UserProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
