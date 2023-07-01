90a877de8409b00a3977d65e34accb43ba652230
John Gibbons <johngibbons167@gmail.com>
2023-07-01 15:20:33 -0400

    Added a my builds section

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,6 +2,7 @@ import SignIn from "../components/SignIn";
 import { UserContext } from "../components/UserContext";
 import { useContext } from "react";
 import { IUser } from "../types/types";
+import PreviewGrid from "../components/PreviewGrid";
 
 const Profile = () => {
   const userDetails = useContext(UserContext) as IUser;
@@ -12,6 +13,13 @@ const Profile = () => {
       <div className="grid place-items-center">
         <SignIn />
       </div>
+      {userDetails._id && (
+        <PreviewGrid
+          name="My builds:"
+          expandable={true}
+          showFromAuthorID={userDetails._id}
+        />
+      )}
     </div>
   );
 };
