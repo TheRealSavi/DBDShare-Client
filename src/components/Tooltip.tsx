cc2fec67b000ffecbfd8a02849e034f197e49fca
John Gibbons <johngibbons167@gmail.com>
2023-08-28 18:31:38 -0400

    Changed to origin top

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -10,8 +10,8 @@ const Tooltip = (props: IToolTip) => {
     <span
       className={
         props.requireHover
-          ? "absolute w-auto p-1 -mt-3 min-w-max transition-all duration-150 scale-0 origin-center rounded-md shadow-md text-white bg-gradient-to-r from-cyan-900 to-blue-900 text-xs font-bold z-50 group-hover:scale-100"
-          : "absolute w-auto p-1 -mt-3 min-w-max transition-all duration-150 scale-100 origin-center rounded-md shadow-md text-white bg-gradient-to-r from-cyan-900 to-blue-900 text-xs font-bold z-50 "
+          ? "absolute w-auto p-1 -mt-3 min-w-max transition-all duration-150 scale-0 origin-top rounded-md shadow-md text-white bg-gradient-to-r from-cyan-900 to-blue-900 text-xs font-bold z-50 group-hover:scale-100"
+          : "absolute w-auto p-1 -mt-3 min-w-max transition-all duration-150 scale-100 origin-top rounded-md shadow-md text-white bg-gradient-to-r from-cyan-900 to-blue-900 text-xs font-bold z-50 "
       }
     >
       {props.children}
