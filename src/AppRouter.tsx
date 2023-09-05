ffe21e39128f2e3917ccd5cf4fe98e90cc94af42
John Gibbons <johngibbons167@gmail.com>
2023-09-04 20:29:42 -0400

    Now dynamically route user to own profile

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -7,18 +7,33 @@ import Profile from "./pages/Profile";
 import Randomizer from "./pages/Randomizer";
 import AuthorPage from "./pages/AuthorPage";
 import GenCalcPage from "./pages/GenCalcPage";
+import PerkInfoPage from "./pages/PerkInfoPage";
+import { useContext } from "react";
+import { UserContext } from "./components/UserContext";
+import { IUser } from "./types/types";
 
 function AppRouter() {
+  const userDetails = useContext(UserContext) as IUser;
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
+      {userDetails._id ? (
+        <Route
+          path="/profile"
+          element={<AuthorPage authorID={userDetails._id} />}
+        />
+      ) : (
+        <Route path="/profile" element={<Profile />} />
+      )}
+
       <Route path="/callback" element={<Profile />} />
       <Route path="/home" element={<Home />} />
       <Route path="/saved" element={<Saved />} />
       <Route path="/createnew" element={<CreateNew />} />
       <Route path="/randomizer" element={<Randomizer />} />
       <Route path="/author/:id" element={<AuthorPage />} />
+      <Route path="/perk/:id" element={<PerkInfoPage />} />
       <Route path="/calc/gen" element={<GenCalcPage />} />
     </Routes>
   );
---
+++ src/pages/PerkInfoPage.tsx
@@ -0,0 +1,48 @@
+import { useMatch } from "react-router-dom";
+import { IPerk } from "../types/types";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import PerkInfo from "../components/PerkInfo";
+import PerkSlot from "../components/PerkSlot";
+
+const PerkInfoPage = () => {
+  const match = useMatch("/perk/:id");
+  const { id } = match?.params ?? {};
+  const [perkData, setPerkData] = useState<IPerk>();
+
+  const fetchPerkData = async (perkId: string) => {
+    try {
+      // Make an API call to fetch the missing perk data using the perk._id or any identifier
+      const response = await axios.get(
+        import.meta.env.VITE_API_URL + "perk/" + perkId
+      );
+      const data = response.data;
+      return data;
+    } catch (error) {
+      console.error("Error fetching perk data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPerkData(id ? id : "").then((data) => {
+      setPerkData({ ...data });
+    });
+  }, [id]);
+
+  return (
+    <div className="flex place-content-center pt-5 flex-col">
+      <PerkSlot
+        perk={perkData}
+        key={0}
+        slotNumber={0}
+        isSelected={false}
+        allowHover={false}
+      ></PerkSlot>
+      <div className="flex place-content-center">
+        <PerkInfo perkData={perkData} />
+      </div>
+    </div>
+  );
+};
+
+export default PerkInfoPage;
