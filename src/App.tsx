4649fcb6e936ddf7e1fb0e115903153a53d06882
TheRealSavi <johngibbons167@gmail.com>
2023-10-05 16:36:54 -0400

    added configprovider for antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,47 @@
 import { useState } from "react";
 import AppRouter from "./AppRouter";
 import Navbar from "./components/Navbar";
+import { ConfigProvider, theme } from "antd";
 
 function App() {
   const [navbarShown, setNavbarShown] = useState(true);
 
   return (
-    <div className="min-h-screen">
-      {navbarShown && <Navbar />}
-      <div className={navbarShown ? "site-content-nav" : "site-content"}>
-        <AppRouter />
-        <div className="fixed bottom-2 pl-4 w-full sm:invisible">
-          <button
-            onClick={() => {
-              setNavbarShown(!navbarShown);
-            }}
-            className="button2"
-          >
-            {navbarShown ? "<<" : ">>"}
-          </button>
+    <ConfigProvider
+      theme={{
+        algorithm: theme.darkAlgorithm,
+        components: {
+          Pagination: {
+            itemActiveBg: "rgba(255,255,255,0.8)",
+          },
+        },
+        token: {
+          colorText: "rgba(255,255,255,1)",
+          colorPrimary: "rgba(0,0,0,1)",
+          colorBgTextActive: "rgba(255,255,255,1)",
+          colorFillAlter: "rgba(255,255,255,1)",
+          colorTextDisabled: "rgba(255,255,255,0.4)",
+          colorBgContainer: "rgba(255,255,255,0.15)",
+        },
+      }}
+    >
+      <div className="min-h-screen">
+        {navbarShown && <Navbar />}
+        <div className={navbarShown ? "site-content-nav" : "site-content"}>
+          <AppRouter />
+          <div className="fixed bottom-2 pl-4 w-full sm:invisible">
+            <button
+              onClick={() => {
+                setNavbarShown(!navbarShown);
+              }}
+              className="button2"
+            >
+              {navbarShown ? "<<" : ">>"}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </ConfigProvider>
   );
 }
 
